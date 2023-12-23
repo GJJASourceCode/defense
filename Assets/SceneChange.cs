a7292fca9027beedb1e1578137f4f9ade28f2881
jindosz <127918186+jindosz@users.noreply.github.com>
2023-12-24 00:26:06 +0900

    ddd

--- Assets/SceneChange.cs
+++ Assets/SceneChange.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class GameScenesMove : MonoBehaviour
+public class SceneChange : MonoBehaviour
 {
     public void GameScenesCtrl()
     {
